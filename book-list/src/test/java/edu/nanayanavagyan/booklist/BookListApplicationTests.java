8510a07d8b347e8a879852bc051ed263e15a324c
armennanayan <127252750+armennanayan@users.noreply.github.com>
2023-04-17 21:46:41 -0400

    Add files via upload

---
+++ book-list/src/test/java/edu/nanayanavagyan/booklist/BookListApplicationTests.java
@@ -0,0 +1,13 @@
+package edu.nanayanavagyan.booklist;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class BookListApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
