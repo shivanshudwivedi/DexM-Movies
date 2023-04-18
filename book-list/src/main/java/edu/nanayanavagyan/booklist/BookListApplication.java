c84ca86bbcbcd36cf9e94a0deb1bbb39a115016b
armennanayan <127252750+armennanayan@users.noreply.github.com>
2023-04-18 16:00:48 -0400

    Introduced Bean and Autowired

--- book-list/src/main/java/edu/nanayanavagyan/booklist/BookListApplication.java
+++ book-list/src/main/java/edu/nanayanavagyan/booklist/BookListApplication.java
@@ -2,9 +2,16 @@ package edu.nanayanavagyan.booklist;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.context.annotation.Bean;
+import org.springframework.web.client.RestTemplate;
 
 @SpringBootApplication
 public class BookListApplication {
+
+	@Bean
+	public RestTemplate getRestTemplate(){
+		return new RestTemplate();
+	}
 
 	public static void main(String[] args) {
 		SpringApplication.run(BookListApplication.class, args);
--- book-list/src/main/java/edu/nanayanavagyan/booklist/controller/BookListController.java
+++ book-list/src/main/java/edu/nanayanavagyan/booklist/controller/BookListController.java
@@ -4,6 +4,7 @@ import java.util.Arrays;
 import java.util.List;
 import java.util.stream.Collectors;
 
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
@@ -16,13 +17,16 @@ import edu.nanayanavagyan.booklist.models.Rating;
 @RestController
 @RequestMapping("/bookList")
 public class BookListController {
-    
+
+    @Autowired
+    private RestTemplate restTemplate;
+
+
     
     @RequestMapping("/{readerId}")
     public List<BookUnit> getBooks(@PathVariable("readerId") String userId) {
 
-        RestTemplate restTemplate = new RestTemplate();
-
+        
         //get all rated book IDs
         List<Rating> ratings = Arrays.asList(
             new Rating("1", 4),
