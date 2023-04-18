9578e6a1da0f1be216a3a8046af0b80ffdbc2551
armennanayan <127252750+armennanayan@users.noreply.github.com>
2023-04-17 21:50:12 -0400

    Add files via upload

---
+++ book-infromation/src/test/java/edu/nanayanavagyan/bookinfromation/BookInfromationApplicationTests.java
@@ -0,0 +1,13 @@
+package edu.nanayanavagyan.bookinfromation;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class BookInfromationApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
