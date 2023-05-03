e91966c93b6bab4997b57d290ccb41825cc00b6b
anahitavagyan <anahitavagyan10@gmail.com>
2023-05-03 16:13:52 -0400

    Ratings info
    Hard coded the objects for the Ratings in the book-ratings service

--- book-infromation/src/main/java/edu/nanayanavagyan/bookinfromation/controller/BookController.java
+++ book-infromation/src/main/java/edu/nanayanavagyan/bookinfromation/controller/BookController.java
@@ -1,8 +1,6 @@
 package edu.nanayanavagyan.bookinfromation.controller;
 
 import java.util.ArrayList;
-import java.util.Arrays;
-import java.util.List;
 
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PathVariable;
--- book-ratings/src/main/java/edu/nanayanavagyan/bookratings/controller/BookRatingsController.java
+++ book-ratings/src/main/java/edu/nanayanavagyan/bookratings/controller/BookRatingsController.java
@@ -1,5 +1,6 @@
 package edu.nanayanavagyan.bookratings.controller;
 
+import java.util.ArrayList;
 import java.util.Arrays;
 import java.util.List;
 
@@ -13,21 +14,103 @@ import edu.nanayanavagyan.bookratings.models.ReaderRating;
 @RestController
 @RequestMapping("/ratings")
 public class BookRatingsController {
+     //creating the list
+     public ArrayList<Rating> bookRatings = new ArrayList<Rating>();
+    
+     //creating new objects
+     Rating book1 = new Rating(1, "Anna Karenina", 4);
+     Rating book2 = new Rating(2, "Normal People", 4);
+     Rating book3 = new Rating(3, "The Lord of the Rings", 5);
+     Rating book4 = new Rating(4, "To Kill a Mockingbird", 5);
+     Rating book5 = new Rating(5, "The Book of Five Rings",5);
+     Rating book6 = new Rating(6, "Pride and Prejudice", 5);
+     Rating book7 = new Rating(7, "The Picture of Dorian Gray", 4);
+     Rating book8 = new Rating(8, "Life of Pi", 3);
+     Rating book9 = new Rating(9, "Moby-Dick", 4);
+     Rating book10 = new Rating(10, "Persepolis",5);
+     
+     //adding the books to the list 
+     public void addBook(ArrayList<Rating> bookRatings){
+        bookRatings.add(book1);
+        bookRatings.add(book2);
+        bookRatings.add(book3);
+        bookRatings.add(book4);
+        bookRatings.add(book5);
+        bookRatings.add(book6);
+        bookRatings.add(book7);
+        bookRatings.add(book8);
+        bookRatings.add(book9);
+        bookRatings.add(book10);
+     }
 
     @RequestMapping("/{bookId}")
     public Rating getRating(@PathVariable("bookId") Integer bookId){
-        return new Rating(bookId,"Anna Karenina", 4);
+        addBook(bookRatings);
+        Rating ans = bookRatings.get(1);
+        for(int i = 0; i < bookRatings.size(); i++){
+            if(bookId == bookRatings.get(i).getBookId()){
+                ans =  bookRatings.get(i);
+            }
+        }
+        return ans;
+
+        
     }
 
     @RequestMapping("reader/{readerId}")
-    public ReaderRating getReaderRating(@PathVariable("readerId") String readerId){
-        List<Rating> readerRatings = Arrays.asList(
-            new Rating(1, "Anna Karenina", 4),
-            new Rating(2, "Normal People", 5)
-        );
-         ReaderRating readerRating =new ReaderRating();
-         readerRating.setReaderRating(readerRatings);
-         return readerRating;
+    public ReaderRating getReaderRating(@PathVariable("readerId") int readerId){
+        if(readerId == 1){
+            List<Rating> readerRatings = Arrays.asList(
+                new Rating(1, "Anna Karenina", 4),
+                new Rating(4, "To Kill a Mockingbird", 5)
+            );
+            
+            ReaderRating readerRating =new ReaderRating();
+            readerRating.setReaderRating(readerRatings);
+            return readerRating;
+        }else if(readerId == 2){
+            List<Rating> readerRatings = Arrays.asList(
+            new Rating(3, "The Lord of the Rings", 5),
+            new Rating(5, "The Book of Five Rings", 5),
+            new Rating(6, "Pride and Prejudice", 5)
+            );
+            ReaderRating readerRating =new ReaderRating();
+            readerRating.setReaderRating(readerRatings);
+            return readerRating;
+        }else if(readerId == 3){
+            List<Rating> readerRatings = Arrays.asList(
+            new Rating(2, "Normal People", 4)
+            );
+            
+            ReaderRating readerRating =new ReaderRating();
+            readerRating.setReaderRating(readerRatings);
+            return readerRating;
+        }else if(readerId == 4){
+            List<Rating> readerRatings = Arrays.asList(
+            new Rating(7, "The Picture of Dorian Gray", 4),
+            new Rating(8, "Life of Pi", 3),
+            new Rating(9, "Moby-Dick", 4)
+            );
+
+            ReaderRating readerRating =new ReaderRating();
+            readerRating.setReaderRating(readerRatings);
+            return readerRating;
+        }else if(readerId == 5){
+            List<Rating> readerRatings = Arrays.asList(
+            new Rating(10, "Persepolis", 5)
+            );
+            
+
+            ReaderRating readerRating =new ReaderRating();
+            readerRating.setReaderRating(readerRatings);
+            return readerRating;
+        }else{
+            return null;
+        }
+        
+         
+        
+        
     }
 
     
