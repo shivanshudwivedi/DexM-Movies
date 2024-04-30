98eb70798c52425abcdc7ee2b1b1f97b50c07ad7
viethungpham <hung.pham@trincoll.edu>
2024-04-30 16:26:42 -0400

    Add homepage to backend

--- MovieBackend.py
+++ MovieBackend.py
@@ -15,6 +15,53 @@ def authenticate_guest():
     return jsonify(response.json())
 
 # Here add the other routes we need for the backend
+@app.route('/api/movie/rate/<int:movie_id>', methods=['POST'])
+def rate_movie(movie_id):
+    rating = request.json.get('value')
+    url = f'https://api.themoviedb.org/3/movie/{movie_id}/rating'
+    headers = {
+        'accept': 'application/json',
+        'Content-Type': 'application/json;charset=utf-8',
+        'Authorization': f'Bearer {API_KEY}'
+    }
+    data = {'value': rating}
+    try:
+        response = requests.post(url, headers=headers, json=data)
+        return jsonify(response.json())
+    except Exception as e:
+        print('Error rating movie:', e)
+        return jsonify({'error': 'Error rating movie'})
 
+@app.route('/api/tv/rate/<int:tv_show_id>', methods=['POST'])
+def rate_tv_show(tv_show_id):
+    rating = request.json.get('value')
+    url = f'https://api.themoviedb.org/3/tv/{tv_show_id}/rating'
+    headers = {
+        'accept': 'application/json',
+        'Content-Type': 'application/json;charset=utf-8',
+        'Authorization': f'Bearer {API_KEY}'
+    }
+    data = {'value': rating}
+    try:
+        response = requests.post(url, headers=headers, json=data)
+        return jsonify(response.json())
+    except Exception as e:
+        print('Error rating TV show:', e)
+        return jsonify({'error': 'Error rating TV show'})
+    
+@app.route('/api/movies/popular', methods=['GET'])
+def fetch_movies():
+    url = 'https://api.themoviedb.org/3/movie/popular?language=en-US&page=1'
+    headers = {'Authorization': f'Bearer {API_KEY}'}
+    response = requests.get(url, headers=headers)
+    return jsonify(response.json())
+
+@app.route('/api/tv/popular', methods=['GET'])
+def fetch_tv_shows():
+    url = 'https://api.themoviedb.org/3/tv/popular?language=en-US&page=1'
+    headers = {'Authorization': f'Bearer {API_KEY}'}
+    response = requests.get(url, headers=headers)
+    return jsonify(response.json())
+    
 if __name__ == '__main__':
     app.run()
